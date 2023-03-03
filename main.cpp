d24934a00e75fa342920af61e4248def21d30abd
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-03-03 21:04:13 +0300

    Working conception (2 widgets)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "overlay.h"
+#include "menu.h"
 
 #include <QApplication>
 
@@ -10,11 +11,18 @@ int main(int argc, char *argv[])
     overlay.setAttribute(Qt::WA_NoSystemBackground, true);
     overlay.setAttribute(Qt::WA_TranslucentBackground, true);
     overlay.setAttribute(Qt::WA_TransparentForMouseEvents, true);
-
     overlay.setWindowFlags(Qt::FramelessWindowHint);
     overlay.setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
 
     overlay.showFullScreen();
+
+    Menu menu;
+    menu.setAttribute(Qt::WA_TransparentForMouseEvents, false);
+    menu.setAttribute(Qt::WA_NoSystemBackground, true);
+    menu.setAttribute(Qt::WA_TranslucentBackground, true);
+    menu.setWindowFlags(Qt::FramelessWindowHint);
+    menu.setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    menu.show();
 
     return application.exec();
 }

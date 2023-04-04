8d9787094271b7a7d7348186edc4844bd430f1a1
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-04-04 12:04:05 +0300

    Update dllmain.cpp

--- dll/src/dllmain.cpp
+++ dll/src/dllmain.cpp
@@ -1,6 +1,12 @@
 #include <Windows.h>
 #include <iostream>
 
+uintptr_t WINAPI mainthread(HMODULE hModule) {
+    AllocConsole();
+    std::cout << "dll cout" << std::endl;
+    //FreeConsole();
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule,
                       DWORD  ul_reason_for_call,
                       LPVOID lpReserved
@@ -9,11 +15,10 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH: {
-//        HANDLE thread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, 0);
-//        if (thread)
-//            CloseHandle(thread);
-//        break;
-        std::cout << "dll cout" << std::endl;
+        HANDLE thread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)mainthread, hModule, 0, 0);
+        if (thread)
+            CloseHandle(thread);
+        break;
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:

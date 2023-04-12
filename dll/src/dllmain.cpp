ce39eb43fed6ca2d24ab53c328da1e0eceae2c6b
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-04-12 21:53:18 +0300

    Fixed thread in dllmain

--- dll/src/dllmain.cpp
+++ dll/src/dllmain.cpp
@@ -4,9 +4,11 @@
 uintptr_t WINAPI mainthread(HMODULE hModule) {
     AllocConsole();
 
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
 
-    std::cout << "dll cout" << std::endl;
-    //FreeConsole();
+    std::cout << "[INFO] Console allocated succesfully" << std::endl;
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
@@ -17,17 +19,9 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH: {
-            // causes segfault. idc
-//        HANDLE thread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)mainthread, hModule, 0, 0);
-//        if (thread)
-//            CloseHandle(thread);
-        AllocConsole();
-
-        FILE* f;
-        freopen_s(&f, "CONOUT$", "w", stdout);
-
-        std::cout << "[INFO] Console allocated succesfully" << std::endl;
-
+        HANDLE thread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)mainthread, hModule, 0, 0);
+        if(thread)
+            CloseHandle(thread);
         break;
     }
     case DLL_THREAD_ATTACH:

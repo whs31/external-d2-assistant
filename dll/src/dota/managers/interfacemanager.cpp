246894798348bf9d6dff1ac62c47e2280fb8e131
Рязанцев Дмитрий <75864292+whs31@users.noreply.github.com>
2023-04-15 00:00:10 +0300

    Test iterate over entity list

--- dll/src/dota/managers/interfacemanager.cpp
+++ dll/src/dota/managers/interfacemanager.cpp
@@ -17,6 +17,15 @@ void InterfaceManager::findInterfaces()
     source2client = Memory::findInterface<Memory::VirtualClass>("client.dll", "Source2Client002");
 
     gameEntitySystem = *Memory::Address(source2client->getVirtualFunction(25).pointer).getAbsoluteAddress<CGameEntitySystem**>(3, 7);
-    std::cout << "[INTERFACE MANAGER] Found CGameEntitySystem, " << gameEntitySystem->getHighestEntityIndex();
+    std::cout << "[INTERFACE MANAGER] Found CGameEntitySystem, " << gameEntitySystem->getHighestEntityIndex() << std::endl;
+
+    // это просто тестовая итерация по энтити. ее можно убрать, нужна чисто для отладки
+    CEntityIdentity* start = gameEntitySystem->getIdentity(gameEntitySystem->getHighestEntityIndex());
+    std::cout << "Start: " << start->name() << std::endl;
+    while(start->m_pNext)
+    {
+        start = start->m_pNext;
+        std::cout << start->name() << std::endl;
+    }
 }
 
